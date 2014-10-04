bd5fdd3f9c8916b51c269c682dbebb42629051b1
Robert Keizer <robert.keizer@quipped.ca>
2014-10-04 01:12:52 -0400

    Some more stuffs.

--- log.js
+++ log.js
@@ -4,6 +4,22 @@ var nano	= require( "nano" )( config.couchdb_url );
 var db		= nano.use( config.db );
 
 var app = express( );
+
+app.param( 'id', function( req, res, cb, id ){
+	req.id = id;
+	cb( );
+} );
+
+app.param( "attachment_name", function( req, res, cb, attachment_name ){
+	req.attachment_name = attachment_name;
+	cb ();
+} );
+
+app.use( function( req, res, cb ){
+	//console.log( req.path );
+	cb( );
+} );
+
 app.get( '/insert', function( req, res ){
 	
 	db.insert( req.query, function( err, dbRes ){
@@ -22,6 +38,28 @@ app.get( "/get", function( req, res ){
 		}
 
 		return res.json( body.rows );
+	} );
+} );
+
+app.get( "/get//:id", function( req, res ){
+	db.get( req.id, function( err, dbRes ){
+		if( err ){
+			return res.json( err );
+		}
+		return res.json( dbRes );
+	} );
+} );
+
+app.get( "/get///:id/:attachment_name", function( req, res ){
+	db.attachment.get( req.id, req.attachment_name ).pipe( res );
+} );
+
+app.get( "/posts", function( req, res ){
+	db.view( "posts", "by-time", function( err, body ){
+		if( err ){
+			return res.json( err );
+		}
+		return res.json( body );
 	} );
 } );
 
